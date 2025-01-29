7751d65199b99f130c20488174ae589e14c092f9
acalcutt <acalcutt@techidiots.net>
2025-01-28 22:40:57 -0500

    try to make dynamic chunk size

--- rio_rgbify/mbtiler.py
+++ rio_rgbify/mbtiler.py
@@ -276,20 +276,30 @@ class RGBTiler:
 
         with self.db:
             self.db.add_bounds_center_metadata(bounds, self.min_z, self.max_z, self.encoding, self.format, "Terrain")
-
             
             with ctx.Pool(processes, initializer=self._init_worker) as pool:
                 try:
                     total_processed = 0
-                    for i, result in enumerate(pool.imap_unordered(process_func, tiles, chunksize=batch_size), 1):
-                        if result:
-                            self.db.insert_tile_with_retry(*result, use_inverse_y=True)
-                            total_processed += 1
-                            print(f"Processed {total_processed}/{total_tiles} tiles")
+                    
+                    tiles_iter = iter(tiles)  # Create an iterator for tiles
+                    
+                    while True:
+                        current_tiles = list(itertools.islice(tiles_iter, batch_size))
+                        if not current_tiles:
+                            break  # Break if no more tiles
+                            
+                        #Adjust chunk size based on the remaining tiles, and the amount of available processes
+                        chunk_size = max(1, min(batch_size, len(current_tiles) // processes or len(current_tiles)))
+
+
+                        for i, result in enumerate(pool.imap_unordered(process_func, current_tiles, chunksize=chunk_size),1):
+                            if result:
+                                self.db.insert_tile_with_retry(*result, use_inverse_y=True)
+                                total_processed += 1
+                                print(f"Processed / tiles")
                         
-                        if i % batch_size == 0 or i == total_tiles:   # Commit after each batch or at the end
-                            self.db.conn.commit()
-                            print("Committed to database")
+                        self.db.conn.commit()
+                        print("Committed to database")
 
                     print(f"Completed processing {total_processed} tiles")
                 
